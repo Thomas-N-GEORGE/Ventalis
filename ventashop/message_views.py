64258eba417346d6ce2a90cc279c8ba7e127e5dc
Thomas GEORGE <tgeorge.latelierduson@gmail.com>
2023-05-22 09:13:34 +0200

    Fixed conversation security issue.

--- ventashop/message_views.py
+++ ventashop/message_views.py
@@ -1,7 +1,8 @@
 """Our message and conversation related views' module."""
 
+from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView
@@ -25,23 +26,17 @@ class MessageListView(
 
     form_class = MessageForm
 
-    def get_queryset(self, *args, **kwargs):
-        """Message list (aka one conversation) to be displayed."""
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        """Check if user is a participant in conversation."""
 
         user = self.request.user
+        conversation = get_object_or_404(Conversation, pk=self.kwargs["pk"])
+        print(conversation.participants.all())
 
-        # n last messages to be displayed.
-        if "n_last" in self.kwargs:
-            n_last = int(self.kwargs["n_last"])
-            m_set = list(Message.objects.filter(conversation__id=self.kwargs["pk"]))[
-                -n_last:
-            ]
+        if user not in conversation.participants.all():
+            return HttpResponse("Unauthorized", status=401)
 
-        # all messages to be displayed.
-        else:
-            m_set = Message.objects.filter(conversation__id=self.kwargs["pk"])
-
-        return m_set
+        return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         """Handle new message POST request."""
@@ -57,6 +52,22 @@ class MessageListView(
         return HttpResponseRedirect(
             reverse("ventashop:messages-last", args=(conversation.id, 5))
         )
+
+    def get_queryset(self, *args, **kwargs):
+        """Message list (aka one conversation) to be displayed."""
+
+        # n last messages to be displayed.
+        if "n_last" in self.kwargs:
+            n_last = int(self.kwargs["n_last"])
+            m_set = list(Message.objects.filter(conversation__id=self.kwargs["pk"]))[
+                -n_last:
+            ]
+
+        # all messages to be displayed.
+        else:
+            m_set = Message.objects.filter(conversation__id=self.kwargs["pk"])
+
+        return m_set
 
     def get_context_data(self, **kwargs):
         """Related conversation and new message form to be displayed."""
